78acf8c816476779aa95e1b0b35a12e7bb4a7db1
rvarga09 <146484780+rvarga09@users.noreply.github.com>
2025-02-20 19:49:57 +0000

    landing page

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+        <div>
+            {/* style={{marginRight: spacing + 'em'}} */}
+
+
+            <div className="text-center p-4 text-white" style={{backgroundColor: 'black'}}>
+                © 2021 Copyright:
+                
+            </div>
+        </div>
+    )
+}
+
+export default Footer
